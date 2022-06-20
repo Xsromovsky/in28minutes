1bcb9f6e11af775e3c7c098e544e02c6c70d1bb6
samuel <samuel14sromovsky@gmail.com>
2022-06-20 22:40:58 +0200

    initial commit

---
+++ src/main/java/com/example/rest/webservices/in28minutes/In28minutesApplication.java
@@ -0,0 +1,13 @@
+package com.example.rest.webservices.in28minutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class In28minutesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(In28minutesApplication.class, args);
+    }
+
+}
