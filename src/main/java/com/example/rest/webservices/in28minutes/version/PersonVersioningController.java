77d62fc1c541dda935fe60173e1c9c279005eab7
samuel <samuel14sromovsky@gmail.com>
2022-06-22 22:38:28 +0200

    added header versioning and param versionning

--- src/main/java/com/example/rest/webservices/in28minutes/version/PersonVersioningController.java
+++ src/main/java/com/example/rest/webservices/in28minutes/version/PersonVersioningController.java
@@ -11,8 +11,34 @@ public class PersonVersioningController {
     public PersonV1 personV1(){
         return new PersonV1("samuel sromo");
     }
-    @GetMapping("/v2/person")
-    public PersonV2 personV2(){
+
+    @GetMapping(value = "/person/param", params = "param-version=1")
+    public PersonV1 paramV1(){
+        return new PersonV1("samuel sromo");
+    }
+
+    @GetMapping(value = "/person/param", params = "param-version=2")
+    public PersonV2 paramV2(){
+        return new PersonV2(new Name("samo", "sromovsky"));
+    }
+
+    @GetMapping(value = "/person/header", headers = "version=1")
+    public PersonV1 headerV1(){
+        return new PersonV1("samuel sromo");
+    }
+
+    @GetMapping(value = "/person/header", headers = "version=2")
+    public PersonV2 headerV2(){
+        return new PersonV2(new Name("samo", "sromovsky"));
+    }
+
+    @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 producesV1(){
+        return new PersonV1("samuel sromo");
+    }
+
+    @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 producesV2(){
         return new PersonV2(new Name("samo", "sromovsky"));
     }
 }
